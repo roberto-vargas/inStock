cd5a91fff8f20db0338237f3e67115bf9d650efa
honestykim <honestykim@gmail.com>
2014-11-05 14:33:25 -0800

    Cleaned up arduino code

--- ArduinoCode/inStockArduino/inStockArduino/inStockArduino.ino
+++ ArduinoCode/inStockArduino/inStockArduino/inStockArduino.ino
@@ -1,30 +1,3 @@
-/*
-  Button
- 
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
- 
- 
- The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Button
- */
-
-// constants won't change. They're used here to 
 // set pin numbers:
 const int buttonPin2 = 2;     // the number of the pushbutton pin
 const int buttonPin3 = 3;
@@ -54,10 +27,19 @@ int buttonState12 = 0;
 int buttonState13 = 0;
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);  
+    // initialize the pushbutton pin as an input:
+  pinMode(buttonPin2, INPUT); 
+  pinMode(buttonPin3, INPUT);   
+  pinMode(buttonPin4, INPUT);  
+  pinMode(buttonPin5, INPUT); 
+  pinMode(buttonPin6, INPUT); 
+  pinMode(buttonPin7, INPUT); 
+  pinMode(buttonPin8, INPUT); 
+  pinMode(buttonPin9, INPUT); 
+  pinMode(buttonPin10, INPUT); 
+  pinMode(buttonPin11, INPUT); 
+  pinMode(buttonPin12, INPUT); 
+  pinMode(buttonPin13, INPUT); 
 
   Serial.begin(9600);
 
